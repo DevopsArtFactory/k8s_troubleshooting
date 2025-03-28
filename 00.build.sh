ea4be5df08e411ccb97d763b16f1d6072b2dea16
K Shooter <kshooter2000@gmail.com>
2025-03-28 01:59:15 +0000

    kshooter2000

--- 00.build.sh
+++ 00.build.sh
@@ -1,12 +1,12 @@
-# docker build -f Dockerfile-52 -t javalove93/hello .
-# docker push javalove93/hello
+docker build -f Dockerfile-52 -t kshooter2000/hello .
+docker push kshooter2000/hello
 
-# docker build -f Dockerfile-53 -t javalove93/hello-http-server .
-# docker push javalove93/hello-http-server
+docker build -f Dockerfile-53 -t kshooter2000/hello-http-server .
+docker push kshooter2000/hello-http-server
 
-docker build -f Dockerfile-54 -t javalove93/flask-hello-world .
-docker push javalove93/flask-hello-world
+docker build -f Dockerfile-54 -t kshooter2000/flask-hello-world .
+docker push kshooter2000/flask-hello-world
 
-# gcc -static -o hello_world src/hello_world.c
-# docker build -f Dockerfile-57 -t javalove93/hello-without-shell .
-# docker push javalove93/hello-without-shell
+gcc -static -o hello_world src/hello_world.c
+docker build -f Dockerfile-57 -t kshooter2000/hello-without-shell .
+docker push kshooter2000/hello-without-shell
