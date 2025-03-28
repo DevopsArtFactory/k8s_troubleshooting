07f8aff285e175e9245bb77c2eb1a5f25a5d1c67
K Shooter <kshooter2000@gmail.com>
2025-03-28 11:01:26 +0900

    updated

--- 00.build.sh
+++ 00.build.sh
@@ -1,12 +1,12 @@
-docker build -f Dockerfile-52 -t kshooter2000/hello .
-docker push kshooter2000/hello
+# docker build -f Dockerfile-52 -t kshooter2000/hello .
+# docker push kshooter2000/hello
 
-docker build -f Dockerfile-53 -t kshooter2000/hello-http-server .
-docker push kshooter2000/hello-http-server
+# docker build -f Dockerfile-53 -t kshooter2000/hello-http-server .
+# docker push kshooter2000/hello-http-server
 
 docker build -f Dockerfile-54 -t kshooter2000/flask-hello-world .
 docker push kshooter2000/flask-hello-world
 
-gcc -static -o hello_world src/hello_world.c
-docker build -f Dockerfile-57 -t kshooter2000/hello-without-shell .
-docker push kshooter2000/hello-without-shell
+# gcc -static -o hello_world src/hello_world.c
+# docker build -f Dockerfile-57 -t kshooter2000/hello-without-shell .
+# docker push kshooter2000/hello-without-shell
