2a2139961e693cd455212b15663ae666296da7a2
K Shooter <kshooter2000@gmail.com>
2025-03-30 14:51:30 +0900

    updated

--- create_minikube.sh
+++ create_minikube.sh
@@ -1,3 +1,3 @@
-minikube start --cpus=2 --memory=4096 --nodes 3 -p minikube-multi-nodes
+minikube start --cpus=2 --memory=4096 --nodes 3 -p minikube-multi-nodes --extra-config=kubelet.register-with-taints=node-role.kubernetes.io/control-plane=:NoSchedule
 
 minikube addons enable metrics-server -p minikube-multi-nodes
