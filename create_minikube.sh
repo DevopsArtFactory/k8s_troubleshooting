48a67b053eea46fe49d0adce74cf7e01b853f17b
K Shooter <kshooter2000@gmail.com>
2025-03-30 21:11:01 +0900

    updated

--- create_minikube.sh
+++ create_minikube.sh
@@ -1,3 +1,8 @@
-minikube start --cpus=2 --memory=4096 --nodes 3 -p minikube-multi-nodes --extra-config=kubelet.register-with-taints=node-role.kubernetes.io/control-plane=:NoSchedule
+minikube start --cpus=4 --memory=8192 -p minikube-multi-nodes 
 
 minikube addons enable metrics-server -p minikube-multi-nodes
+
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+
+minikube node add -p minikube-multi-nodes
+
