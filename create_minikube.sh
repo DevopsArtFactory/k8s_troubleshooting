84eff21c7ab1212dc33824b2b3aed715bd40cb72
K Shooter <kshooter2000@gmail.com>
2025-03-30 21:14:25 +0900

    updated

--- create_minikube.sh
+++ create_minikube.sh
@@ -1,2 +1,6 @@
 minikube start --cpus=4 --memory=8192 
 
+minikube addons enable metrics-server
+
+helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
+
